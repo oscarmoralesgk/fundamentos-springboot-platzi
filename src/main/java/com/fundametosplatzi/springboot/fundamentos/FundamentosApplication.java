a9f651ef97e18be6c06ee606e6bc858628fcba16
oscar.morales <oscar.morales@grupokonecta.com>
2022-02-22 09:58:54 -0500

    first commit

---
+++ src/main/java/com/fundametosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundametosplatzi.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
